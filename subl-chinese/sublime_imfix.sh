ee8fcc82b9e5c5d62b83949fdf661077ab833e54
guimaodai <wyyxdgm@163.com>
2016-02-18 18:09:02 +0800

    curdir

--- subl-chinese/sublime_imfix.sh
+++ subl-chinese/sublime_imfix.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-
+__dirname=`pwd`
 if [ -f /usr/share/applications/sublime-text.desktop ]; then
-sudo apt-get install libgtk2.0-dev libglade2-dev firefox-dev libchm-dev libssl-dev
-cd ~/Documents/subl-chinese
+	sudo apt-get install libgtk2.0-dev libglade2-dev firefox-dev libchm-dev libssl-dev
+	cd $__dirname
 
-sudo gcc -shared -o libsublime-imfix.so sublime_imfix.c  `pkg-config --libs --cflags gtk+-2.0` -fPIC
+	sudo gcc -shared -o libsublime-imfix.so sublime_imfix.c  `pkg-config --libs --cflags gtk+-2.0` -fPIC
 
-sudo mv libsublime-imfix.so /opt/sublime_text/
+	sudo mv libsublime-imfix.so /opt/sublime_text/
 
-sudo cat bin_subl > /usr/bin/subl
+	sudo cat bin_subl > /usr/bin/subl
 
-sudo sed -i 's#=/opt/sublime_text/sublime_text\([^\n]*\)#=bash -c "LD_PRELOAD=/opt/sublime_text/libsublime-imfix.so exec /opt/sublime_text/sublime_text\1"#g' /usr/share/applications/sublime-text.desktop
+	sudo sed -i 's#=/opt/sublime_text/sublime_text\([^\n]*\)#=bash -c "LD_PRELOAD=/opt/sublime_text/libsublime-imfix.so exec /opt/sublime_text/sublime_text\1"#g' /usr/share/applications/sublime-text.desktop
 
-cd ~
+	cd ~
 
-echo "success for sublime-text chinese supporting!"
+	echo "success for sublime-text chinese supporting!"
 else
 	echo "no file /usr/share/applications/sublime-text.desktop"
 fi
