f1a8e5cfe58a9d7cc4fe0ff1af91c68f865c8fc6
guimaodai <wyyxdgm@163.com>
2017-09-21 14:58:15 +0800

    ok

--- nvm.sh
+++ nvm.sh
@@ -1,7 +1,7 @@
 
 sudo apt-get install build-essential libssl-dev
-#curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.4/install.sh | bash
-wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.4/install.sh | bash
+#curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh |bash
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
 
 source $profile
 
