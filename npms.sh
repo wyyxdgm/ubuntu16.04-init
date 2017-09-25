51df30a9e23fb340482f26f266a42062f88241af
guimaodai <wyyxdgm@163.com>
2017-09-24 20:11:39 -0700

    npms

--- npms.sh
+++ npms.sh
@@ -1,7 +1,8 @@
 sudo npm install -g cnpm --registry=https://registry.npm.taobao.org
-sudo npm install -g supervisor
-sudo npm install -g node-inspector
-sudo npm install -g grunt gulp bower
-sudo npm install -g mocha
-sudo npm install -g pm2
-sudo npm install -g casperjs
+/bin/bash --login
+cnpm install -g supervisor
+cnpm install -g node-inspector
+cnpm install -g grunt gulp bower
+cnpm install -g mocha
+cnpm install -g pm2
+cnpm install -g casperjs
